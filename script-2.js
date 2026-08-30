5fc39d6a4527d14cef5beab102cb50d3615eacae
Elvira <elvira.burdynska@gmail.com>
2026-08-30 21:03:10 +0300

    Homework L2

---
+++ script-2.js
@@ -0,0 +1,87 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+// const value = -3;
+// // console.log(value > 0);
+// console.log(value <= 0);
+//Перевірте це на варіантах 1, 0, -3.
+// Якщо змінна ="test" - виведіть true,
+// let value;
+// value = "nottest";
+// if (value === "test") {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+//Перевірте це на варіантах 'test', "qwerty", true
+// Якщо змінна більше 10 - відніміть 5,
+
+// let value;
+// value = 9;
+// if (value > 10) {
+//     console.log(value-5);
+// }
+// else {
+//     console.log(value + 5);
+//  }
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// let value = prompt("ВВедіть число від 1 до 12");
+
+// switch (value) {
+//     case "1":
+//         console.log("Січень");
+//         break;
+//     case "2":
+//         console.log("Лютий");
+//         break;
+//     case "3":
+//         console.log("Березень");
+//         break;
+//     case "4":
+//         console.log("Квітень");
+//         break;
+//     case "5":
+//         console.log("Травень");
+//         break;
+//     case "6":
+//         console.log("Червень");
+//         break;
+//     case "7":
+//         console.log("Липень");
+//         break;
+//     case "8":
+//         console.log("Серпень");
+//         break;
+//     case "9":
+//         console.log("Вересень");
+//         break;
+//     case "10":
+//         console.log("Жовтень");
+//         break;
+//     case "11":
+//         console.log("Листопад");
+//         break;
+//     case "12":
+//         console.log("Грудень");
+//         break;
+//     default:
+//         console.log("Невірне число");
+// }
+
+// //Зробіть сервіс який отримує тризначне число
+// let value = Math.floor(Math.random() * 900 + 100);
+
+// console.log(value);
+
+
+
+
+// //Поверніть користувачу сумму цих чисел
+// let hundreds = Math.floor(value / 100);
+// let tens = Math.floor((value % 100) / 10);
+// let units = value % 10;
+// console.log(hundreds + tens + units);
+    
